143c521cdf33290208e4970f59e3df57280a9c53
ban hussein <banhussien97@gmail.com>
2019-09-15 14:58:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
+<<<<<<< HEAD
 const companyName = 'Scuber';
+=======
+let companyName = 'Scuber';
+>>>>>>> b8bee8869c52a8a01c5b99634c9301d4ca4b03db
 let mostProfitableNeighborhood = 'Chelsea';
 let companyCeo = 'Susan Smith';
